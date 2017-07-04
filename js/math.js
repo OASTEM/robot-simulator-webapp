7a8b435f07155e71dadcf35bf2e7f154f742aa02
Peter Yang <me@peteryang.io>
2017-07-04 03:17:22 -0700

    Updated with formulas

--- js/math.js
+++ js/math.js
@@ -2,10 +2,10 @@
 
 // We will putput as x, y, rot
 let output = []
-
-const sideLength = 1;
+let currRotation = currX = currY = 0
+const sideLength = 1
 rawData.forEach((item, index) => {
-    let deltaTime, deltaTheta
+    let deltaTime, deltaTheta, rotToPivot, deltaX, deltaY
     
     // Find delta time
     if (index === 0) {
@@ -16,15 +16,35 @@ rawData.forEach((item, index) => {
     }
     
     // Calculate change of angle
-    deltaTheta = (item.xVelocity - item.yVelocity) / sideLength
-    output.push(deltaTheta)
+    deltaTheta = (item.rVelocity - item.lVelocity) / sideLength
+    
+    // Flip the polarity because Grace Yao said so
+    deltaTheta = -deltaTheta
+    
+    // Update global rotation
+    currRotation += deltaTheta
+    
+    // Rotation degrees to 
+    if (item.rVelocity == item.lVelocity) {
+        rotToPivot = item.rVelocity * deltaTime
+    }
+    rotToPivot = (sideLength / 2) * ((item.rVelocity + item.lVelocity) / (item.rVelocity - item.lVelocity))
+    
+    deltaX = rotToPivot * (Math.cos(deltaTheta + currRotation) - Math.cos(currRotation))
+    deltaY = rotToPivot * (Math.sin(deltaTheta + currRotation) - Math.sin(currRotation))
+    
+    // Negate deltas to make +Y North and +X East
+    deltaX = -deltaX
+    deltaY = -deltaY
+    
+    // Update global positions
+    currX += deltaX
+    currY += deltaY
+    
+    output.push({
+        "rot" : currRotation,
+        "x" : currX,
+        "y" : currY
+    })
 });
 console.log(output)
-
-Math.radians = function(degrees) {
-  return degrees * Math.PI / 180;
-};
-
-Math.degrees = function(radians) {
-  return radians * 180 / Math.PI;
-};
